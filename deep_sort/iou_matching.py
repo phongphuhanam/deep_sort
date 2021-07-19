7c3676d4392403bd83bb322a9c8ed844623723ee
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2021-07-19 10:43:33 +0900

    optimize iou cost function

--- deep_sort/iou_matching.py
+++ deep_sort/iou_matching.py
@@ -2,42 +2,73 @@
 from __future__ import absolute_import
 import numpy as np
 from . import linear_assignment
+try:
+    import numba
+    # @numba.njit()
+    def iou(bbox, candidates):
+        """Computer intersection over union.
 
+        Parameters
+        ----------
+        bbox : ndarray
+            A bounding box in format `(top left x, top left y, width, height)`.
+        candidates : ndarray
+            A matrix of candidate bounding boxes (one per row) in the same format
+            as `bbox`.
 
-def iou(bbox, candidates):
-    """Computer intersection over union.
+        Returns
+        -------
+        ndarray
+            The intersection over union in [0, 1] between the `bbox` and each
+            candidate. A higher score means a larger fraction of the `bbox` is
+            occluded by the candidate.
 
-    Parameters
-    ----------
-    bbox : ndarray
-        A bounding box in format `(top left x, top left y, width, height)`.
-    candidates : ndarray
-        A matrix of candidate bounding boxes (one per row) in the same format
-        as `bbox`.
+        """
+        bbox_tl, bbox_br = bbox[:2], bbox[:2] + bbox[2:]
+        candidates_tl = candidates[:, :2]
+        candidates_br = candidates[:, :2] + candidates[:, 2:]
+        tl = np.maximum(bbox_tl, candidates_tl)
+        br = np.minimum(bbox_br, candidates_br)
+        wh = np.maximum(0, br-tl)
 
-    Returns
-    -------
-    ndarray
-        The intersection over union in [0, 1] between the `bbox` and each
-        candidate. A higher score means a larger fraction of the `bbox` is
-        occluded by the candidate.
+        area_intersection = wh.prod(axis=1)
+        area_bbox = bbox[2:].prod()
+        area_candidates = candidates[:, 2:].prod(axis=1)
+        return area_intersection / (area_bbox + area_candidates - area_intersection)
+except:
+    def iou(bbox, candidates):
+        """Computer intersection over union.
 
-    """
-    bbox_tl, bbox_br = bbox[:2], bbox[:2] + bbox[2:]
-    candidates_tl = candidates[:, :2]
-    candidates_br = candidates[:, :2] + candidates[:, 2:]
+        Parameters
+        ----------
+        bbox : ndarray
+            A bounding box in format `(top left x, top left y, width, height)`.
+        candidates : ndarray
+            A matrix of candidate bounding boxes (one per row) in the same format
+            as `bbox`.
 
-    tl = np.c_[np.maximum(bbox_tl[0], candidates_tl[:, 0])[:, np.newaxis],
-               np.maximum(bbox_tl[1], candidates_tl[:, 1])[:, np.newaxis]]
-    br = np.c_[np.minimum(bbox_br[0], candidates_br[:, 0])[:, np.newaxis],
-               np.minimum(bbox_br[1], candidates_br[:, 1])[:, np.newaxis]]
-    wh = np.maximum(0., br - tl)
+        Returns
+        -------
+        ndarray
+            The intersection over union in [0, 1] between the `bbox` and each
+            candidate. A higher score means a larger fraction of the `bbox` is
+            occluded by the candidate.
 
-    area_intersection = wh.prod(axis=1)
-    area_bbox = bbox[2:].prod()
-    area_candidates = candidates[:, 2:].prod(axis=1)
-    return area_intersection / (area_bbox + area_candidates - area_intersection)
+        """
+        bbox_tl, bbox_br = bbox[:2], bbox[:2] + bbox[2:]
+        candidates_tl = candidates[:, :2]
+        candidates_br = candidates[:, :2] + candidates[:, 2:]
 
+        tl = np.c_[np.maximum(bbox_tl[0], candidates_tl[:, 0])[:, np.newaxis],
+                np.maximum(bbox_tl[1], candidates_tl[:, 1])[:, np.newaxis]]
+        br = np.c_[np.minimum(bbox_br[0], candidates_br[:, 0])[:, np.newaxis],
+                np.minimum(bbox_br[1], candidates_br[:, 1])[:, np.newaxis]]
+        wh = np.maximum(0., br - tl)
+
+        area_intersection = wh.prod(axis=1)
+        area_bbox = bbox[2:].prod()
+        area_candidates = candidates[:, 2:].prod(axis=1)
+        return area_intersection / (area_bbox + area_candidates - area_intersection)
 
 def iou_cost(tracks, detections, track_indices=None,
              detection_indices=None):
